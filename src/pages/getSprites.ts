35b7f5a2016ace0f1e9b7c1dd9bfd327c5d411d3
Matthieu Locussol <dev.matthieu.locussol@gmail.com>
2022-11-20 14:32:23 +0100

    fix: getSprites

--- src/pages/getSprites.ts
+++ src/pages/getSprites.ts
@@ -1,11 +1,11 @@
 import type { APIContext } from "astro";
-import * as fs from "fs";
-import { resolve } from "path";
 
 export async function get({ url: { searchParams } }: APIContext) {
     const id = searchParams.get("id");
-    const file = fs.readFileSync(resolve(`public/spritesJson/${id}.json`));
-    const fileJson = JSON.parse(file.toString());
+    const file = await fetch(
+        `${import.meta.env.PUBLIC_BASE_PATH}/spritesJson/${id}.json`
+    );
+    const fileJson = await file.json();
 
     return {
         body: JSON.stringify(fileJson),
